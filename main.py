90180c49fb77ecebdd3df9138ff9e04bad0a3064
pyork2022 <pyork2022@fau.edu>
2026-03-03 19:42:53 -0500

    Added mlfq function call

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from scheduler import run_fcfs, run_sjf
+from scheduler import run_fcfs, run_sjf, run_mlfq
 
 def main():
     run_fcfs()
     run_sjf()
+    run_mlfq()
+
 
 if __name__ == "__main__":
     main()
